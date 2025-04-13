b6461a038c8aa1eaacbfd455bd366da91bc8314a
Jason Choong <jason@Yenyis-MacBook-Pro.local>
2025-04-13 12:07:07 -0700

    Cleaned up for template

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,9 @@ const client = generateClient<Schema>();
 
 function App() {
   const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+//  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+/*
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -22,18 +23,20 @@ function App() {
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
   }
-
+*/
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>{user?.signInDetails?.loginId}'s account</h1>
+      <h1>My account</h1>
+/*
+      <button onClick={createTodo}>+ new</button> 
       <ul>
         {todos.map((todo) => (<li 
           onClick={() => deleteTodo(todo.id)}
           key={todo.id}>{todo.content}</li>
         ))}
       </ul>
+*/
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
